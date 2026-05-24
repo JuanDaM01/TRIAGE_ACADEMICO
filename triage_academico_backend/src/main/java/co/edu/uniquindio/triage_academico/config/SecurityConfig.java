c64013549f954b8b8ee9d08195c6d8798f645b97
JuanDaM01 <159975691+JuanDaM01@users.noreply.github.com>
2026-05-23 21:06:06 -0500

    AVISO

--- triage_academico_backend/src/main/java/co/edu/uniquindio/triage_academico/config/SecurityConfig.java
+++ triage_academico_backend/src/main/java/co/edu/uniquindio/triage_academico/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package co.edu.uniquindio.triage_academico.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,14 +16,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.security.config.Customizer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
